90ba6c642140fe98daf68e0d82cea18d2e817512
giizy <giovanna.pm2020@gmail.com>
2025-02-25 22:14:37 -0300

    php

---
+++ terceiro.php
@@ -0,0 +1,14 @@
+<?php  
+
+$data = "25 de fevereiro de 2025";
+$salario = 850.00;
+$cargo = "estagiário";
+$idade = 18;
+$resultado = true;
+
+if ($resultado){
+	echo('Verdadeiro');
+	echo "<br>";
+	echo "Seu salário é " . $salario;
+	}
+?>
